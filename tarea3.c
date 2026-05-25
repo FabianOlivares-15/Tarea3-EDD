71977fcfdf1be8b4aeaa5fa018c70874842fafcc
FabianOlivares-15 <fabianolivaresp15@gmail.com>
2026-05-25 00:23:29 +0000

    Descripción de los cambios realizados

--- tarea3.c
+++ tarea3.c
@@ -72,15 +72,15 @@ State *transition(State *estado_actual, int accion, int maze[N][N]){
     nuevo->y = estado_actual->y;
     nuevo->steps = estado_actual->steps + 1; // le sumamos un paso mas
     nuevo->parent = estado_actual; // dejamos como padre al actual, para saber luego el camino recorrido
-    if(accion == arriba) nuevo->x -= 1;
+    if(accion == arriba) nuevo->x -= 1; // se mueve segun la opcion que se elige
     else if(accion == abajo) nuevo->x += 1;
     else if(accion == derecha) nuevo->y += 1;
     else if(accion == izquierda) nuevo->y -= 1;
-    if(nuevo->x < 0 || nuevo->x >= N || nuevo->y < 0 || nuevo->y >= N){
+    if(nuevo->x < 0 || nuevo->x >= N || nuevo->y < 0 || nuevo->y >= N){ // se valida para que no se salga del maze
         free(nuevo);
         return NULL;
     }
-    if(maze[nuevo->x][nuevo->y] != 0){
+    if(maze[nuevo->x][nuevo->y] != 0){ // se valida que no choque
         free(nuevo);
         return NULL;
     }
@@ -88,26 +88,26 @@ State *transition(State *estado_actual, int accion, int maze[N][N]){
 }
 
 List *get_adjacent_nodes(State* estado_actual, int maze[N][N]){
-    List *adj = list_create();
-    for(int accion = 1; accion <= 4; accion ++){
-        State *nuevo = transition(estado_actual, accion, maze);
-        if(nuevo != NULL){
+    List *adj = list_create(); // crea lista adyacentes
+    for(int accion = 1; accion <= 4; accion ++){ // recorremos en los movimientos
+        State *nuevo = transition(estado_actual, accion, maze);  // generamos el estado
+        if(nuevo != NULL){ //si fue valido lo agregamos a la lista
             list_pushBack(adj, nuevo);
         }
     }
     return adj;
 }
 void imprimirRutaFinal(State *estado_final, int maze[N][N]){
-    int copiaMaze[N][N];
+    int copiaMaze[N][N]; //creamos una copia de la original para no modificar la original
     for(int i = 0; i < N; i++)
         for(int j = 0; j < N; j++)
             copiaMaze[i][j] = maze[i][j];
-    State *actual = estado_final;
+    State *actual = estado_final; //nos ubicamos en el ultimo y vamos retrocediendo y marcamos camino
     while(actual != NULL){
         copiaMaze[actual->x][actual->y] = 2;
         actual = actual->parent;
     }
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++){ //marcamos nuestro camino con un caracter por ejemplo +
         for(int j = 0; j < N; j++){
             if(i == 0 && j == 0) printf(" I ");
             else if(i == N-1 && j == N-1) printf(" M ");
@@ -119,14 +119,14 @@ void imprimirRutaFinal(State *estado_final, int maze[N][N]){
     }
 }
 void dfs(State *estado_actual, int maze[N][N]){
-    Stack *pila = stack_create(NULL);
-    int visitado[N][N] = {0};
-    int cont = 0;
-    stack_push(pila, estado_actual);
-    while(stack_top(pila) != NULL){
-        State *actual = (State*)stack_pop(pila);
-        cont++;
-        if(is_final_state(actual)){
+    Stack *pila = stack_create(NULL); //creamos la pila 
+    int visitado[N][N] = {0}; // usamos una matriz con ceros para marcar los visitados
+    int cont = 0; //creamos un contador para las iteraciones
+    stack_push(pila, estado_actual); //agregamos a la pila el incio 
+    while(stack_top(pila) != NULL){ 
+        State *actual = (State*)stack_pop(pila); //sacamos de la pila
+        cont++; //aumenta el contador
+        if(is_final_state(actual)){ // si llegamos al final, imprimimos la la ruta encontrado, mostramos pasos e iteraciones
             printf("\n Busqueda en profundidad\n");
             imprimirRutaFinal(actual, maze);
             printf("Pasos desde el principio: %d\n", actual->steps);
@@ -134,15 +134,15 @@ void dfs(State *estado_actual, int maze[N][N]){
             free(pila);
             return;
         }
-        if(visitado[actual->x][actual->y] == 1){
+        if(visitado[actual->x][actual->y] == 1){ //revisamos si ya pasamos por ahi entonces no lo tomamos y avanzamos
             free(actual);
             continue;
         }
-        visitado[actual->x][actual->y] = 1;
-        List *adyacentes = get_adjacent_nodes(actual, maze);
-        State *adyacenteValido = (State*)list_popFront(adyacentes);
-        while(adyacenteValido != NULL){
-            if(visitado[adyacenteValido->x][adyacenteValido->y] == 0){
+        visitado[actual->x][actual->y] = 1; // lo marcamos como visitado
+        List *adyacentes = get_adjacent_nodes(actual, maze); //obtenemos los adyacentes
+        State *adyacenteValido = (State*)list_popFront(adyacentes); 
+        while(adyacenteValido != NULL){ //recorremos adyacentes
+            if(visitado[adyacenteValido->x][adyacenteValido->y] == 0){ // si no esta visitado lo metemos a la pila
                 stack_push(pila, adyacenteValido);
             }
             else free(adyacenteValido);
@@ -155,14 +155,14 @@ void dfs(State *estado_actual, int maze[N][N]){
     free(pila);
 }
 void bfs(State *estado_actual, int maze[N][N]){
-    Queue *cola = queue_create(NULL);
-    int visitado[N][N] = {0};
-    int cont = 0;
-    queue_insert(cola, estado_actual);
+    Queue *cola = queue_create(NULL); //creamos la cola
+    int visitado[N][N] = {0}; // usamos una matriz con ceros para marcar los visitados
+    int cont = 0; //creamos contador de iteraciones
+    queue_insert(cola, estado_actual); //agregamos a la cola el estado actual
     while(queue_front(cola) != NULL){
-        State *actual = (State*)queue_remove(cola);
-        cont++;
-        if(is_final_state(actual)){
+        State *actual = (State*)queue_remove(cola); //sacamos de la cola
+        cont++; //aumentamos el contador
+        if(is_final_state(actual)){ // si llegamos al final, imprimimos la la ruta encontrado, mostramos pasos e iteraciones
             printf("\n Busqueda en anchura\n");
             imprimirRutaFinal(actual, maze);
             printf("Pasos desde el principio: %d\n", actual->steps);
@@ -170,15 +170,15 @@ void bfs(State *estado_actual, int maze[N][N]){
             free(cola);
             return;
         }
-        if(visitado[actual->x][actual->y] == 1){
+        if(visitado[actual->x][actual->y] == 1){ //revisamos si ya pasamos por ahi entonces no lo tomamos y avanzamos
             free(actual);
             continue;
         }
-        visitado[actual->x][actual->y] = 1;
-        List *adyacentes = get_adjacent_nodes(actual,maze);
+        visitado[actual->x][actual->y] = 1; //lo marcamos como visitado
+        List *adyacentes = get_adjacent_nodes(actual,maze); //obtenemos los adyacentes
         State *adyacenteValido = (State*)list_popFront(adyacentes);
-        while(adyacenteValido != NULL){
-            if(visitado[adyacenteValido->x][adyacenteValido->y] == 0){
+        while(adyacenteValido != NULL){ //recorremos los adyacentes
+            if(visitado[adyacenteValido->x][adyacenteValido->y] == 0){ //si no esta visitado lo metemos a la cola
                 queue_insert(cola, adyacenteValido);
             }
             else free(adyacenteValido);
@@ -193,17 +193,17 @@ void bfs(State *estado_actual, int maze[N][N]){
 }
 
 void best_first(State *estado_actual, int maze[N][N]){
-    Heap *heap = heap_create();
-    int visitado[N][N] = {0};
-    int cont = 0;
+    Heap *heap = heap_create(); //creamos el heap
+    int visitado[N][N] = {0}; // usamos una matriz con ceros para marcar los visitados
+    int cont = 0; //creamos contador de iteraciones
     int pasos = estado_actual->steps;
-    int prioridad = -(pasos + distancia_L1(estado_actual));
-    heap_push(heap, estado_actual, prioridad);
+    int prioridad = -(pasos + distancia_L1(estado_actual)); //calculamos la prioridad sumando pasos + distanciaL1, se deja en negativo 
+    heap_push(heap, estado_actual, prioridad); //se agrega al heap
     while(heap_top(heap) != NULL){
-        State *actual = (State*)heap_top(heap);
+        State *actual = (State*)heap_top(heap); //sacamos el mejor por prioridad
         heap_pop(heap);
-        cont++;
-        if(is_final_state(actual)){
+        cont++; //aumentamos el contador
+        if(is_final_state(actual)){ // si llegamos al final, imprimimos la la ruta encontrado, mostramos pasos e iteraciones
             printf("\nBusqueda A* encontrada\n");
             imprimirRutaFinal(actual, maze);
             printf("Pasos desde el principio: %d\n", actual->steps);
@@ -211,15 +211,15 @@ void best_first(State *estado_actual, int maze[N][N]){
             free(heap);
             return;
         }
-        if(visitado[actual->x][actual->y] == 1){
+        if(visitado[actual->x][actual->y] == 1){ //revisamos si ya pasamos por ahi entonces no lo tomamos y avanzamos
             free(actual);
             continue;
         }
-        visitado[actual->x][actual->y] = 1;
-        List *adyacentes = get_adjacent_nodes(actual, maze);
+        visitado[actual->x][actual->y] = 1; //lo marcamos como visitado
+        List *adyacentes = get_adjacent_nodes(actual, maze); //obtenemos los adyacentes
         State *adyacenteValido = (State*)list_popFront(adyacentes);
-        while(adyacenteValido != NULL){
-            if(visitado[adyacenteValido->x][adyacenteValido->y] == 0){
+        while(adyacenteValido != NULL){ //recorremos los adyacentes
+            if(visitado[adyacenteValido->x][adyacenteValido->y] == 0){ //si no esta visitado, calculamos su prioridad, se agrega al heap
                 int steps = adyacenteValido->steps;
                 int prio = -(steps + distancia_L1(adyacenteValido));
                 heap_push(heap, adyacenteValido, prio);
